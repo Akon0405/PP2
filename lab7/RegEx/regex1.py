8675eb53031c557b006bb926a8c508b3550b5034
Akon0405 <145010060+Akon0405@users.noreply.github.com>
2023-11-09 16:25:58 +0600

    Add files via upload

--- lab7/RegEx/regex1.py
+++ lab7/RegEx/regex1.py
@@ -1,1 +1,12 @@
-# Write a Python program that matches a string that has an 'a' followed by zero or more 'b''s.
+import re
+file=open('row.txt', 'r', encoding="utf-8")
+text = file.read()
+x=re.findall('ab*', text)
+print(x)
+ 
+if x:
+    print(x)
+    print("Found")
+else:
+    print("Not Found")
+file.close()
